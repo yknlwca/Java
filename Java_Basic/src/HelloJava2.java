a24cce67d116806733d0fde8f4b6b605cba214d5
KL.Choi <145303968+yknlwca@users.noreply.github.com>
2023-12-28 18:59:01 +0900

    first start Java

---
+++ Java_Basic/src/HelloJava2.java
@@ -0,0 +1,9 @@
+public class HelloJava2 {
+
+    public static void main(String[] args){
+        System.out.println("hello java");
+        System.out.println("hello java2");
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+    }
+}
