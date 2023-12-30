3e9a3533f8b19629b22123b9d441aa0c8e1940cc
KL.Choi <145303968+yknlwca@users.noreply.github.com>
2023-12-30 17:31:40 +0900

    update

---
+++ Java_Basic/src/Var8.java
@@ -0,0 +1,16 @@
+public class Var8 {
+    public static void main(String[] args) {
+
+        // 정수
+        byte b = 127; // -128 ~ 127
+        short s = 32767; // -32768 ~ 32767
+        int i = 2147283647; // 약 -20억 ~ 20억
+
+        long l = 9222222222222222L;
+
+        // 실수
+        float f = 10.0f;
+        double d = 10.0;
+
+    }
+}
---
+++ Java_Basic/src/loop/While1_1.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class While1_1 {
+    public static void main(String[] args) {
+        int count = 0;
+        count += 1;
+        System.out.println("헌재 숫자는: "+ count);
+        count += 1;
+        System.out.println("헌재 숫자는: "+ count);
+        count += 1;
+        System.out.println("헌재 숫자는: "+ count);
+    }
+}
