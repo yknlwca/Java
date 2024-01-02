0c69904706966c59230879ae9fcea5cfc30516f9
KL.Choi <145303968+yknlwca@users.noreply.github.com>
2024-01-02 13:34:11 +0900

    SSAFY First Day

---
+++ Java_Basic/src/loop/ex/NestedEx2.java
@@ -0,0 +1,19 @@
+package loop.ex;
+
+public class NestedEx2 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println();
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println();
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.println();
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+    }
+}
